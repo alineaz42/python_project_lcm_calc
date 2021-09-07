178e4788701799fbba10656b3054dbb19fc44ad2
Ali Neaz <neazali77@gmail.com>
2021-09-08 01:15:08 +0600

    added the readme.md file

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
 
-def lcm(a, b):
-    pass
+def lcm(m, n, r):
+    maxNum = max(m, n, r)
+    while True:
+        if maxNum % m == 0 and maxNum % n == 0 and maxNum % r == 0:
+            break
+        maxNum = maxNum+1
+    return f"The LCM of {m} and  {n} and {r} is {maxNum}"
 
 
 # taking inputs
 m = int(input("Enter the first Number: "))
 n = int(input("Enter the second number: "))
-print(m, n)
+r = int(input("Enter the third number: "))
+# print(m, n)
+Num = lcm(m, n, r)
+print(Num)
+# maxNum = max(m, n)
+# while True:
+#     if maxNum % m == 0 and maxNum % n == 0:
+#         break
+#     maxNum = maxNum+1
+# print(f"The lcm of {m} and {n} is {maxNum}")
